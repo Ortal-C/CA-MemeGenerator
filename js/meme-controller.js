bbc48a1ab8c293da4fcfdc2c8e67a45453b0eb13
Ortal <ortalcn@mta.ac.il>
2021-10-27 13:40:25 +0300

    load img to canvas

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,20 +1,49 @@
 'use strict';
 
-function onInit(){
-    renderImgs();
+var gElCanvas;
+var gCanvasContext;
+
+function onInit() {
+	gElCanvas = document.getElementById('canvas');
+	gCanvasContext = gElCanvas.getContext('2d');
+	console.log(gCanvasContext);
+	addEventListeners();
+	renderImgs();
 }
 
-function renderImgs(){
-    var images = getImages();
-    var keywords = getKeywords();
-    const strImgsHtmls = images.map((img)=> {
-        return `<img data-imgId="${img.id}" src="${img.url}" alt="meme-#${img.id}" onclick="onImgClick(${img.id})"/>`
-    })
-    document.querySelector('.main-grid').innerHTML = strImgsHtmls.join('');
+function addEventListeners() {
+	// gElCanvas.addEventListener('', function(){
+	//     alert('mouse up ')
+	// })
 }
 
-function onImgClick(idx){
-    document.querySelector('.main-grid').classList.add('hide');
-    console.log(idx);
-    console.log(gImgs[idx-1]); 
+function canvasClicked(ev) {
+	console.log(ev);
+}
+
+function renderImgs() {
+	var images = getImages();
+	var keywords = getKeywords();
+	const strImgsHtmls = images.map((img) => {
+		return `<img data-imgId="${img.id}" src="${img.url}" alt="meme-#${img.id}" onclick="onImgClick(${img.id}, event)"/>`;
+	});
+	document.querySelector('.main-grid').innerHTML = strImgsHtmls.join('');
+}
+
+function onImgClick(id) {
+	document.querySelector('.main-grid').classList.add('hide');
+    //createMeme(id)
+    console.log(createMeme(id));
+	var imgUrl = getImgById(id).url;
+    loadImgToCanvas(imgUrl);
+}
+
+function loadImgToCanvas(imgUrl) {
+	var selectedImg = new Image();
+	selectedImg.src = imgUrl;
+	selectedImg.onload = function () {
+		gElCanvas.width = selectedImg.width;
+		gElCanvas.height = selectedImg.height;
+		gCanvasContext.drawImage(selectedImg, 0, 0);
+	};
 }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,49 +1,57 @@
 'use strict';
 
 var gKeywords = {
-    'happy': 1,
-    'sad': 1,
-    'laugh': 1,
-    'sleep': 1,
-    'sarcastic': 1,
-    'funny': 1,
-    'animals':1,
-    'akward':1,
-    'love':1,
-    'drinks':1,
-    'kids':1,
-    'cute':1
-}
+	happy: 1,
+	sad: 1,
+	laugh: 1,
+	sleep: 1,
+	sarcastic: 1,
+	funny: 1,
+	animals: 1,
+	akward: 1,
+	love: 1,
+	drinks: 1,
+	kids: 1,
+	cute: 1,
+};
 
 var gImgs = [];
-var gMeme = { 
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
-}
+var gMeme;
 
 _createImgs();
 
-function _createImgs(){
-    for (let i = 1; i <= 18; i++) {
-        gImgs.push({id: i, url: `img/meme-imgs/${i}.jpg`, keywords: ['happy']})
-    }
+function _createImgs() {
+	for (let i = 1; i <= 18; i++) {
+		gImgs.push({ id: i, url: `img/meme-imgs/${i}.jpg`, keywords: ['happy'] });
+	}
 }
-function getKeywords(){
-    return gKeywords;
-}
-
-function getImages(){
-    return gImgs;
+function getKeywords() {
+	return gKeywords;
 }
 
-function getMeme(){
-    return gMeme;
+function getImages() {
+	return gImgs;
+}
+
+function getImgById(id) {
+	return gImgs[id - 1];
+}
+
+function getMeme() {
+	return gMeme;
+}
+
+function createMeme(id) {
+	return {
+		selectedImgId: id,
+		selectedLineIdx: 0,
+		lines: [
+			{
+				txt: 'Text goes here...',
+				size: 20,
+				align: 'left',
+				color: 'red',
+			},
+		],
+	};
 }
